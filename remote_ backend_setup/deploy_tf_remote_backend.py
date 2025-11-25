eca9b4ef8ba7e2c855f2a3a037f80c243ba58f73
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2025-11-25 21:38:27 +0200

    Create storage account and blob container

--- remote_ backend_setup/deploy_tf_remote_backend.py
+++ remote_ backend_setup/deploy_tf_remote_backend.py
@@ -1,5 +1,5 @@
-import os
 import logging
+import os
 
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
@@ -15,26 +15,88 @@ def main():
     storage_account_name = "sptxntfstr"
     blob_container_name = "tfstate"
 
-    rg_name = provision_resource_group(
-        credential, subscription_id, rg_name, location)
+    rg_name = provision_resource_group(credential, subscription_id, rg_name, location)
+
+    provision_storage_account(
+        credential,
+        subscription_id,
+        storage_account_name,
+        rg_name,
+        location,
+        blob_container_name,
+    )
 
 
-def provision_resource_group(credential, subscription_id: str, rg_name: str, location: str) -> str:
-    """ Creates a resource group in a specified location"""
+def provision_resource_group(
+    credential, subscription_id: str, rg_name: str, location: str
+) -> str:
+    """Creates a resource group in a specified location"""
 
     try:
 
         resource_client = ResourceManagementClient(credential, subscription_id)
 
         rg_result = resource_client.resource_groups.create_or_update(
-            rg_name, {"location": location})
+            rg_name, {"location": location}
+        )
         logging.info(
-            f"Provisioned resource group {rg_result.name} in the {rg_result.location} region.")
+            f"Provisioned resource group {rg_result.name} in the {rg_result.location} region."
+        )
 
         return rg_result.name
     except Exception as e:
-        logging.exception(
-            f"An error occurred creating the resource group: {e}")
+        logging.exception(f"An error occurred creating the resource group: {e}")
+
+
+def provision_storage_account(
+    credential,
+    subscription_id: str,
+    storage_account_name: str,
+    rg_name: str,
+    location: str,
+    blob_container_name: str,
+):
+    """Create storage account and blob container"""
+
+    try:
+
+        storage_client = StorageManagementClient(credential, subscription_id)
+
+        # Check availability of storage account name
+        availability_result = storage_client.storage_accounts.check_name_availability(
+            {"name": storage_account_name}
+        )
+
+        if not availability_result.name_available:
+            logging.warning(
+                f"Storage name {storage_account_name} is already in use. Try another name"
+            )
+
+        poller = storage_client.storage_accounts.begin_create(
+            rg_name,
+            storage_account_name,
+            {
+                "location": location,
+                "kind": "StorageV2",
+                "sku": {"name": "Standard_LRS"},
+            },
+        )
+
+        account_result = poller.result()
+        logging.info(f"Provisioned storage account {account_result.name}")
+
+    except Exception as e:
+        logging.exception(f"An error occurred creating storage account: {e}")
+
+    try:
+        container_name = blob_container_name
+        container = storage_client.blob_containers.create(
+            rg_name, storage_account_name, container_name, {}
+        )
+        logging.info(f"Provisioned blob container: {e}")
+
+    except Exception as e:
+        logging.exception(f"An error occurred creating blob container: {e}")
 
 
 if __name__ == "__main__":
