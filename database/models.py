e82c76b422d08eb947a14eb335301baa74a7b51a
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2025-11-27 21:34:30 +0200

    Generate migration for transactions table

--- database/models.py
+++ database/models.py
@@ -1,6 +1,7 @@
 import os
 import uuid
 from datetime import date, datetime
+from decimal import Decimal
 from typing import Optional
 
 from sqlalchemy import BigInteger, SmallInteger
@@ -17,11 +18,24 @@ class Cards(SQLModel, table=True):
     cvv: int = Field(default=None, sa_column=Column(SmallInteger()))
     has_chip: str
     num_cards_issued: int = Field(default=None, sa_column=Column(SmallInteger()))
-    credit_limit : int = Field(default=None, sa_column=Column(BigInteger()))
+    credit_limit: int = Field(default=None, sa_column=Column(BigInteger()))
     acct_open_date: date
     year_pin_last_changed: int = Field(default=None, sa_column=Column(SmallInteger()))
     card_on_dark_web: str
 
+
+class Transactions(SQLModel, table=True):
+    id: uuid.UUID | None = Field(primary_key=True)
+    date: date
+    client_id: int = Field(default=None, sa_column=Column(SmallInteger()))
+    card_id: int = Field(default=None, sa_column=Column(SmallInteger()))
+    amount: Decimal = Field(default=0, max_digits=18, decimal_places=2)
+    use_chip: str
+    merchant_id: int
+    merchant_city: str
+    merchant_state: str
+    zip: int
+    mcc: int
 
 
 db_password = os.environ.get("DB_PASSWORD")
