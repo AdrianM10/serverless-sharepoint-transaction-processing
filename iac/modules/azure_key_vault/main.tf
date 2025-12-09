cd3a6b08fc3cd8d2a0e53393b28cb5132808c510
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2025-12-09 18:52:05 +0200

    Reference tenant_id from azurerm data source

--- iac/modules/azure_key_vault/main.tf
+++ iac/modules/azure_key_vault/main.tf
@@ -2,12 +2,14 @@ data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_key_vault" "kv" {
   name                        = var.kv_name
   resource_group_name         = data.azurerm_resource_group.rg.name
   location                    = data.azurerm_resource_group.rg.location
   enabled_for_disk_encryption = true
-  tenant_id                   = ""
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
   rbac_authorization_enabled  = true
