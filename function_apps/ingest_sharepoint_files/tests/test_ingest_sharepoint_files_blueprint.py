9b63f33d514fb1a19937511adbac8a1ca4742aea
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2026-01-11 18:17:56 +0200

    Add test for processing users_data

--- function_apps/ingest_sharepoint_files/tests/test_ingest_sharepoint_files_blueprint.py
+++ function_apps/ingest_sharepoint_files/tests/test_ingest_sharepoint_files_blueprint.py
@@ -3,7 +3,11 @@ import pandas as pd
 import math
 import os
 
-from ingest_sharepoint_files_blueprint import process_transactions, process_cards
+from ingest_sharepoint_files_blueprint import (
+    process_transactions,
+    process_cards,
+    process_users,
+)
 from models import Transactions, Cards, Users
 from sqlmodel import Session, SQLModel, create_engine, select, delete
 
@@ -40,8 +44,7 @@ def users_data():
 
 @pytest.fixture
 def transactions_data():
-    transactions = pd.read_excel(
-        open(FILE_PATH, "rb"), sheet_name="transactions")
+    transactions = pd.read_excel(open(FILE_PATH, "rb"), sheet_name="transactions")
 
     return transactions
 
@@ -112,27 +115,24 @@ def test_transactions_data(transactions_data):
     assert set(transactions_data.columns) == set(expected_columns)
 
 
-def test_process_transactions(session: Session, transactions_data):
-
-    statement = select(Transactions).where(Transactions.source == FILE_PATH)
+def test_process_users(session: Session, users_data):
+    statement = select(Users).where(Users.source == FILE_PATH)
     results = session.exec(statement).all()
 
     assert len(results) == 0
 
-    process_transactions(transactions_data, FILE_PATH)
-
-    statement = select(Transactions).where(Transactions.source == FILE_PATH)
+    process_users(users_data, FILE_PATH)
+    statement = select(Users).where(Users.source == FILE_PATH)
     results = session.exec(statement).all()
 
     assert len(results) == 5
 
-    # Delete all records that were inserted into table
-    session.exec(delete(Transactions).where(Transactions.source == FILE_PATH))
+    # Delete all records from table post assert
+    session.exec(delete(Users).where(Users.source == FILE_PATH))
     session.commit()
 
 
 def test_process_cards(session: Session, cards_data):
-
     statement = select(Cards).where(Cards.source == FILE_PATH)
     results = session.exec(statement).all()
 
@@ -147,4 +147,22 @@ def test_process_cards(session: Session, cards_data):
 
     # Delete all records that were inserted into table
     session.exec(delete(Cards).where(Cards.source == FILE_PATH))
+    session.commit()
+
+
+def test_process_transactions(session: Session, transactions_data):
+    statement = select(Transactions).where(Transactions.source == FILE_PATH)
+    results = session.exec(statement).all()
+
+    assert len(results) == 0
+
+    process_transactions(transactions_data, FILE_PATH)
+
+    statement = select(Transactions).where(Transactions.source == FILE_PATH)
+    results = session.exec(statement).all()
+
+    assert len(results) == 5
+
+    # Delete all records that were inserted into table
+    session.exec(delete(Transactions).where(Transactions.source == FILE_PATH))
     session.commit()
